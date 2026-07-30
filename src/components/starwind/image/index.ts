3096d8634918fa4009ef7ab622d38995280237cc
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-07-29 20:55:07 -0400

    chore: remove old files and start fresh, initialize new astro7 frontend

--- src/components/starwind/image/index.ts
+++
@@ -1,9 +0,0 @@
-import Image, { image } from './Image.astro';
-
-const ImageVariants = {
-  image,
-};
-
-export { Image, ImageVariants };
-
-export default Image;
--- src/components/starwind/prose/index.ts
+++
@@ -1,9 +0,0 @@
-import Prose, { prose } from './Prose.astro';
-
-const ProseVariants = {
-  prose,
-};
-
-export { Prose, ProseVariants };
-
-export default Prose;
