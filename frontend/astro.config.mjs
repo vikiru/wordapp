b604d42eea69784ee5a8b60d854d7bf1d184c63e
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-08-06 23:40:14 -0400

    chore: add stricter tsconfig settings and validation of data files in build step

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-	
 	vite: {
 		plugins: [tailwindcss()],
+		build: {
+			sourcemap: false,
+		},
 	},
 });
