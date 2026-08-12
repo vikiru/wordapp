25c4a627a5e57c8c08dbc6af7ddf6cddc301b1e9
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-08-12 15:34:54 -0400

    chore: update deps and config files

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		build: {
-			sourcemap: false,
-		},
-	},
+  site: 'https://wordapp.pages.dev', // change once Cloudflare Pages is setup
+  integrations: [
+    sitemap(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      sourcemap: false,
+    },
+  },
 });
